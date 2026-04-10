d7f33d1d655672db519fb291e128156028529e56
Andrey <aklaus.code@gmail.com>
2026-04-10 08:38:39 +0300

    prettyprint library added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <prettyprint.h>
 
-int main() {
-    std::cout << "hello" << std::endl;
-    return 0;
+const int DOC_WIDTH = 80;
+
+auto main() -> int {
+  auto doc = pp::group(pp::text("void") + pp::softbreak() + pp::text("main()"));
+  auto settings = std::cout << pp::set_width(DOC_WIDTH);
+  settings << doc << std::endl;
+
+  return 0;
 }
